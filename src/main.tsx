abca2b1569f9fd329900fee6ba8229fcbe2f359f
Sean Wotherspoon <seanjeemon@gmail.com>
2026-07-23 14:19:23 -0700

    Remove GitHub Pages configuration

--- src/main.tsx
+++ src/main.tsx
@@ -4,18 +4,6 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 
-(function normalizeRedirect() {
-  const url = new URL(window.location.href);
-  const redirect = url.searchParams.get("redirect");
-
-  if (redirect) {
-    const targetPath =
-      redirect.startsWith("/") ? redirect : `/${redirect}`;
-
-    window.history.replaceState(null, "", targetPath);
-  }
-})();
-
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
